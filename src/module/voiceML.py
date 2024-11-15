53542f0b0b7c26867bbae03b7341ab3aa4b2ee06
pakuland3 <pakuland_1@naver.com>
2024-11-15 20:24:05 +0900

    1115

---
+++ src/module/voiceML.py
@@ -0,0 +1,15 @@
+# need to use sklearn(scikit-learn) module and
+
+import sklearn
+
+import whisper
+
+class voiceML:
+    def __init__(self,key:str):
+        pass
+
+    def train_model(self):
+        pass
+
+    def prediction(self):
+        pass
--- talkingRobot.py
+++ talkingRobot.py
@@ -1,4 +1,5 @@
 from src.module.mlforkidsimages import MLforKidsImageProject
+from src.module.voiceML import voiceML
 import src.module.key as moduleKey
 import tkinter as tk
 from tkinter import filedialog
@@ -20,6 +21,8 @@ myproject.train_model()
 
 # print ("result: '%s' with %d%% confidence" % (label, confidence))
 
+global inputImage
+
 def genImage(path,size):
     tmp_img=Image.open(path)
     tmp_img=tmp_img.resize(size)
@@ -32,36 +35,53 @@ def open_image():
     )
     if path:
         inputImage=myproject.prediction(path)
-    canvas.itemconfig(normal_face,state="hidden")
-    # canvas.itemconfig(happy_face,state="hidden")
-    # canvas.itemconfig(sad_face,state="hidden")
-    # canvas.itemconfig(angry_face,state="hidden")
-    # canvas.itemconfig(anxious_face,state="hidden")
+    else:
+        print("path is inavailable")
+        main.quit()
+    for name in displayImage.keys():
+        canvas.itemconfig(displayImageId[name],state="hidden")
+    canvas.itemconfig(displayImageId[inputImage["class_name"]],state="normal")
+    for name in displayText.keys():
+        canvas.itemconfig(displayTextId[name],state="hidden")
+    canvas.itemconfig(displayTextId[inputImage["class_name"]],state="normal")
     
 main=tk.Tk()
 main.title("나만의 로봇 친구")
 main.geometry("700x700")
+
+# disable fullscreen
 main.resizable(False, False)
 main.attributes("-fullscreen", False)
 
 canvas=tk.Canvas(main,width=700,height=700)
 
-normal_face=genImage("C:\\codes\\ProblemSolving\\Introduction-to-AI-projects\\src\\images\\normal_face.webp",(600,600))
-# happy_face=genImage("")
-# sad_face=genImage("")
-# angry_face=genImage("")
-# anxious_face=genImage("")
-canvas.create_image(50,0,anchor="nw",image=normal_face)
+displayImage={"normal":genImage("C:\\codes\\ProblemSolving\\Introduction-to-AI-project\\src\\images\\normal.webp",(600,600)),
+              "happy":genImage("C:\\codes\\ProblemSolving\\Introduction-to-AI-project\\src\\images\\happy.webp",(600,600)),
+              "sad":genImage("C:\\codes\\ProblemSolving\\Introduction-to-AI-project\\src\\images\\sad.webp",(600,600)),
+              "angry":genImage("C:\\codes\\ProblemSolving\\Introduction-to-AI-project\\src\\images\\angry.webp",(600,600)),
+              "anxious":genImage("C:\\codes\\ProblemSolving\\Introduction-to-AI-project\\src\\images\\anxious.webp",(600,600))
+            }
 
-greetStr="안녕! 지금 기분이 어때?"
-happyStr="너가 기뻐하니까 나도 기뻐!"
-sadStr="많이 슬펐구나.. 힘든 일이 있으면 알려줘!"
-angryStr="잠시 명상을 하면서 화를 가라 앉혀보자"
-anxiousStr=""
+displayImageId={"normal":0,
+              "happy":0,
+              "sad":0,
+              "angry":0,
+              "anxious":0
+            }
 
+displayText={"greet":"안녕! 지금 기분이 어때?",
+            "happy":"너가 기뻐하니까 나도 기뻐!",
+            "sad":"많이 슬펐구나.. 힘든 일이 있으면 알려줘!",
+            "angry":"잠시 호흡을 하면서 화를 가라 앉혀보자",
+            "anxious":"많이 힘들구나? 우리 같이 힘내자!"
+            }
 
-
-canvas.create_text(350,650,text=greetStr,font=("Noto Sans KR",20))
+displayTextId={"greet":"안녕! 지금 기분이 어때?",
+            "happy":"너가 기뻐하니까 나도 기뻐!",
+            "sad":"많이 슬펐구나.. 힘든 일이 있으면 알려줘!",
+            "angry":"잠시 호흡을 하면서 화를 가라 앉혀보자",
+            "anxious":"많이 힘드세요? 우리 같이 힘내봐요!"
+            }
 
 
 
@@ -69,7 +89,20 @@ canvas.create_text(350,650,text=greetStr,font=("Noto Sans KR",20))
 btn_open_image=tk.Button(main,text="감정 표현하기",command=open_image)
 
 
+# create image on canvas
+for name in displayImage.keys():
+    displayImageId[name]=canvas.create_image(50,0,anchor="nw",image=displayImage[name])
+    canvas.itemconfig(displayImage[name],state="hidden")
 
+canvas.itemconfig(displayImage["normal"],state="normal")
+
+
+# create text on canvas
+for name in displayText.keys():
+    displayTextId[name]=canvas.create_text(350,650,text=displayText[name],font=("Noto Sans KR",20))
+    canvas.itemconfig(displayTextId[name],state="hidden")
+
+canvas.itemconfig(displayTextId["greet"],state="normal")
 
 # install classes
 btn_open_image.pack()
