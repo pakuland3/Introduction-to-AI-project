e627329fb7b8fc93030ebdf40953e7e1d190eb54
pakuland3 <pakuland4@gmail.com>
2024-11-23 14:02:13 +0900

    1123

--- src/module/voiceML.py
+++ src/module/voiceML.py
@@ -84,34 +84,4 @@ data_dir = "..\\training sources"
 # y_pred = classifier.predict(X_test)
 # print(f"Accuracy: {accuracy_score(y_test, y_pred):.2f}")
 
-# joblib.dump(classifier, "audio_classifier.pkl") # save model
-
-
-# test session
-
-
-# # Load the saved model
-# classifier = joblib.load("audio_classifier.pkl")
-
-# # Preprocess and extract features for the new audio
-# audio = preprocess_audio("test-soon-2.wav")
-# features = extract_whisper_features(audio)
-
-# # Reshape the features and make a prediction
-# features = features.reshape(1, -1)  # Ensure 2D input
-# predicted_label = classifier.predict(features)
-
-# # Map numeric label to class name
-# label_mapping = {0: "happy", 1: "sad", 2: "angry", 3: "anxious"}
-# print(f"Predicted class: {label_mapping[predicted_label[0]]}")
-
-
-# "빠른 갈색 여우가 게으른 개를 뛰어넘습니다."
-
-# "안녕하세요! 오늘 기분은 어떠신가요?"
-
-# "머신 러닝 모델은 라벨이 지정된 데이터셋을 사용하여 학습됩니다."
-
-# "오늘의 기온은 섭씨 23도입니다."
-
-# "와, 이게 실제로 일어나다니 믿을 수 없어요!"
+# joblib.dump(classifier, "audio_classifier.pkl") # save models
